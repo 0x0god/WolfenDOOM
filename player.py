5e3e38942991cb62c7b9d15e614ba692d7c015cf
GABRIEL MOGAJI R <37302024+0x0god@users.noreply.github.com>
2023-04-06 12:19:31 +0100

    added chech_wall

--- player.py
+++ player.py
@@ -42,6 +42,16 @@ class Player:
             self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
         self.angle %= math.tau
 
+
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx), int(self.y)):
+            slef.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy)):
+            self.y += dy
+
     def draw(self):
         pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
                     (self.x * 100 + WIDTH * math.cos(self.angle),

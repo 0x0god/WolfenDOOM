8bdb5e2fad94d4e9a5fdf6ea4c076015b3ffa711
GABRIEL MOGAJI R <37302024+0x0god@users.noreply.github.com>
2023-04-06 11:34:27 +0100

    update game and player class

--- player.py
+++ player.py
@@ -12,7 +12,9 @@ class Player:
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
         dx, dy = 0, 0
-        speed = PLAYER_SPEED
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
 
     def update(self):
         self.movement()

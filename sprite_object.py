a26d2f6657b445f5fefa6b548849f1ebcd35453d
GABRIEL MOGAJI R <37302024+0x0god@users.noreply.github.com>
2023-04-09 11:29:26 +0100

    update main

--- sprite_object.py
+++ sprite_object.py
@@ -60,6 +60,26 @@ class AnimatedSprite(SpriteObject):
         self.animation_time = animation_time
         self.path = path.rsplit('/', 1)[0]
         self.images = self.get_images(self.path)
+        self.animation_time_prev = pg.time.get_ticks()
+        self.animation_trigger = False
+
+    def update(self):
+        super().update()
+        self.check_animation_time()
+        self.animate(self.images)
+
+    def animate(self, images):
+        if self.animation_trigger:
+            images.rotate(-1)
+            self.image = images[0]
+    
+    def check_animation_time(self):
+        self.animation_trigger = False
+        time_now = pg.time.get_ticks()
+        if time_now - self.animation_time_prev > self.animation_time:
+            self.animation_time_prev = time_now
+            self.animation_trigger = True
+
 
     def get_images(self, path):
         images = deque()
@@ -67,4 +87,4 @@ class AnimatedSprite(SpriteObject):
             if os.path.isfile(os.path.join(path, file_name)):
                 img = pg.image.load(path + '/' + file_name).convert_alpha()
                 images.append(img)
-            return images
+        return images
